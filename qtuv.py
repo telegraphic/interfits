7274e37eb7ad56946b370a9868e5ba7037ed44db
Danny Price <dan@thetelegraphic.com>
2013-06-23 13:53:59 -0400

    Added inter-plot navigation by right / left clicking

--- qtuv.py
+++ qtuv.py
@@ -124,11 +124,13 @@ class InterFitsGui(QtGui.QWidget):
         
         self.plot_select = QtGui.QComboBox(self)
         self.plot_select.addItem("Single Baseline")
+        self.plot_select.addItem("Single Baseline: dual pol")
         self.plot_select.addItem("Multi baseline: Bandpass")
         self.plot_select.addItem("Multi baseline: Amplitude")
         self.plot_select.addItem("Multi baseline: Phase")
         self.plot_select.activated.connect(self.updateSpinners)
         
+        self.current_plot = ""
         
         self.axes_select = QtGui.QComboBox(self)
         for v in ['Stokes I','Stokes Q','Stokes U','Stokes V']:
@@ -187,6 +189,89 @@ class InterFitsGui(QtGui.QWidget):
         
         self.setGeometry(300, 300, width, height)   
         self.show()
+        
+        def on_click(event):
+            """Enlarge or restore the selected axis."""
+            ax = event.inaxes
+            
+            if ax is None:
+                # Occurs when a region not in an axis is clicked...
+                return
+                
+            if self.current_plot == 'single':
+                if event.button is 1:
+                    if not self.ax_zoomed:
+                        # Change over to a single baseline plot
+                        try:
+                            self.ax_zoomed = True
+                            ax.set_position([0.1, 0.05, 0.85, 0.80])
+                            ax.set_xlabel("Frequency")
+                            ax.set_ylabel("Time")
+                            
+                            for axis in self.sp_fig.axes:
+                                if axis is not ax:
+                                    axis.set_visible(False)
+                                
+                        except ValueError:
+                            raise
+                            self.sp_fig.canvas.mpl_disconnect(self.fig_connect)
+                    
+                elif event.button is 3:
+                    if self.ax_zoomed:
+                        self.ax_zoomed = False
+                        #self.sp_fig.canvas.mpl_disconnect(self.fig_connect)
+                        self.updatePlot()
+                        
+                else:
+                    # No need to re-draw the canvas if it's not a left or right click
+                    return
+                
+            elif self.current_plot == 'multi':
+                if ax is None:
+                    # Occurs when a region not in an axis is clicked...
+                    return
+                if event.button is 1:
+                    if not self.ax_zoomed:
+                        # Change over to a single baseline plot
+                        try:
+                            ant1, ant2 = ax.get_title().split(" ")
+                        except:
+                            ant1 = int(ax.get_title())  
+                            ant2 = ant1                          
+                        try:
+                            self.spin_ref_ant.setValue(int(ant1))
+                            self.spin_ref_ant2.setValue(int(ant2))
+                            self.plot_select.setCurrentIndex(0)
+                            self.current_plot = 'single'
+                            
+                            self.updatePlot()
+                        except:
+                            raise
+                            self.sp_fig.canvas.mpl_disconnect(self.fig_connect)
+                
+                elif event.button is 3:
+                    if not self.ax_zoomed:
+                        ax.set_position([0.1, 0.1, 0.85, 0.85])
+                        # TODO: fix labelling of zoom plots
+                        ax.set_xlabel("Frequency")
+                        ax.set_ylabel("Time")
+                        self.orig_position = ax.get_position()
+                        for axis in event.canvas.figure.axes:
+                           # Hide all the other axes...
+                           if axis is not ax:
+                               axis.set_visible(False)
+                        self.ax_zoomed=True
+                    else:
+                        self.updatePlot()
+                    
+                else:
+                    # No need to re-draw the canvas if it's not a left or right click
+                    return
+                    
+            event.canvas.draw()
+                
+        
+        self.fig_connect = self.sp_fig.canvas.mpl_connect('button_press_event', on_click)
     
     def createSpinner(self, label, action, smin=0, smax=1, step=1):
         """ Create a QtGui.QSpinner with sensible values """
@@ -218,6 +303,8 @@ class InterFitsGui(QtGui.QWidget):
         ant2: int
             antenna number of second antenna
         """
+        self.current_plot = 'single'
+        
         bls = self.uv.d_uv_data['BASELINE']
         
         if ant1 > ant2: bl_id = 256*ant2 + ant1
@@ -227,8 +314,7 @@ class InterFitsGui(QtGui.QWidget):
         x         = x_data[:,:,0] + 1j * x_data[:,:,1]
         
         fig = self.sp_fig
-        
-
+        self.ax_zoomed = False
         
         figtitle = '%s %s: %s -- %s\n'%(self.uv.telescope, self.uv.instrument, self.uv.source, self.uv.date_obs)
         figtitle += "Baseline %i %i"%(ant1, ant2)
@@ -236,7 +322,7 @@ class InterFitsGui(QtGui.QWidget):
         
         ax = plt.subplot(211)
         x_pow     = np.abs(x)
-        print x_pow.shape
+        #print x_pow.shape
         #x_avg    = np.average(x_pow, axis=0)
         x_max     = np.max(x_pow, axis=0)  
         x_med     = np.median(x_pow, axis=0)
@@ -272,6 +358,104 @@ class InterFitsGui(QtGui.QWidget):
         plt.subplots_adjust(left=0.05, right=0.98, top=0.9, bottom=0.05, wspace=0.25, hspace=0.3)
         
         return fig, ax
+
+    def plot_single_baseline_dual_pol(self, ant1, ant2, axis=0):
+        """ Plot single baseline 
+        
+        ant1: int
+            antenna number of first antenna
+        ant2: int
+            antenna number of second antenna
+        """
+        self.current_plot = 'single'
+        
+        bls = self.uv.d_uv_data['BASELINE']
+        
+        if ant1 > ant2: bl_id = 256*ant2 + ant1
+        else: bl_id = 256*ant1 + ant2
+        
+        x_data    = self.uv.d_uv_data['DATA'][bls == bl_id,0,0,0,:,0]  # Baselines, freq and stokes
+        x         = x_data[:,:,0] + 1j * x_data[:,:,1]
+
+        y_data    = self.uv.d_uv_data['DATA'][bls == bl_id,0,0,0,:,1]  # Baselines, freq and stokes
+        y         = y_data[:,:,0] + 1j * y_data[:,:,1]
+        
+        fig = self.sp_fig
+        self.ax_zoomed = False
+        
+        figtitle = '%s %s: %s -- %s\n'%(self.uv.telescope, self.uv.instrument, self.uv.source, self.uv.date_obs)
+        figtitle += "Baseline %i %i"%(ant1, ant2)
+        fig.suptitle(figtitle, fontsize=18)
+        
+        ax = plt.subplot(221)
+        x_pow     = np.abs(x)
+        x_max     = np.max(x_pow, axis=0)  
+        x_med     = np.median(x_pow, axis=0)
+        x_min     = np.min(x_pow, axis=0)  
+        
+        ax.plot(x_med, label='median')
+        ax.plot(x_min, label='min')
+        ax.plot(x_max, label='max')
+        plt.minorticks_on()
+        plt.xlabel("Frequency")
+        plt.ylabel("Amplitude")
+        plt.title(self.uv.stokes_axis[0])
+        plt.legend()
+
+        ax = plt.subplot(222)
+        y_pow     = np.abs(y)
+        y_max     = np.max(y_pow, axis=0)  
+        y_med     = np.median(y_pow, axis=0)
+        y_min     = np.min(y_pow, axis=0)  
+        
+        ax.plot(y_med, label='median')
+        ax.plot(y_min, label='min')
+        ax.plot(y_max, label='max')
+        plt.minorticks_on()
+        plt.xlabel("Frequency")
+        plt.ylabel("Amplitude")
+        plt.title(self.uv.stokes_axis[1])
+        plt.legend()        
+        
+        ax = plt.subplot(245)
+        plt.imshow(x_pow)
+        plt.title("Amplitude")
+        plt.xlabel("Frequency channel")
+        plt.ylabel("Time")
+        ax.set_aspect(x.shape[1] / x.shape[0] * 3. / 4)
+        plt.colorbar(orientation='horizontal')
+        
+        ax = plt.subplot(246)
+        plt.imshow(np.angle(x))
+        plt.title("Phase")
+        plt.xlabel("Frequency channel")
+        plt.ylabel("Time")
+        ax.set_aspect(x.shape[1] / x.shape[0] * 3. / 4)
+        cbar = plt.colorbar(orientation='horizontal')
+        cbar.set_ticks([-np.pi,-np.pi/2,0,np.pi/2,np.pi-0.05])
+        cbar.set_ticklabels(["$-\pi$","$-\pi/2$",0,"$\pi/2$","$\pi$"])
+        
+        ax = plt.subplot(247)
+        plt.imshow(y_pow)
+        plt.title("Amplitude")
+        plt.xlabel("Frequency channel")
+        plt.ylabel("Time")
+        ax.set_aspect(x.shape[1] / x.shape[0] * 3. / 4)
+        plt.colorbar(orientation='horizontal')
+        
+        ax = plt.subplot(248)
+        plt.imshow(np.angle(y))
+        plt.title("Phase")
+        plt.xlabel("Frequency channel")
+        plt.ylabel("Time")
+        ax.set_aspect(x.shape[1] / x.shape[0] * 3. / 4)
+        cbar = plt.colorbar(orientation='horizontal')
+        cbar.set_ticks([-np.pi,-np.pi/2,0,np.pi/2,np.pi-0.05])
+        cbar.set_ticklabels(["$-\pi$","$-\pi/2$",0,"$\pi/2$","$\pi$"])
+        
+        plt.subplots_adjust(left=0.05, right=0.98, top=0.9, bottom=0.05, wspace=0.25, hspace=0.3)
+        
+        return fig, ax
     
     def plot_visibilities(self, ref_ant=1, axis=0, n_rows=4, n_cols=8, plot_type='phase'):
         """ Plot visibilities
@@ -283,9 +467,12 @@ class InterFitsGui(QtGui.QWidget):
         plot_type: str
             Plot type. Can be amplitude 'amp', phase 'phase'.
         """
+        self.current_plot = 'multi'
+        self.ax_zoomed = False
         
         bls = self.uv.d_uv_data['BASELINE']
-
+        
+        
         # Extract the relevant baselines using a truth array
         bls = bls.tolist()
         bl_ids = self.uv.get_baseline_ids(ref_ant)
@@ -297,49 +484,6 @@ class InterFitsGui(QtGui.QWidget):
         
         # Plot the figure
         fig = self.sp_fig
-        self.ax_zoomed = False
-        
-
-        def on_click(event):
-            """Enlarge or restore the selected axis."""
-            ax = event.inaxes
-            
-            if ax is None:
-                # Occurs when a region not in an axis is clicked...
-                return
-            if event.button is 1:
-                if not self.ax_zoomed:
-                    # Change over to a single baseline plot
-                    try:
-                        ant1, ant2 = ax.get_title().split(" ")
-                        self.spin_ref_ant.setValue(int(ant1))
-                        self.spin_ref_ant2.setValue(int(ant2))
-                        self.plot_select.setCurrentIndex(0)
-                        self.updatePlot()
-                    except ValueError:
-                        self.sp_fig.canvas.mpl_disconnect(self.fig_connect)
-                
-            elif event.button is 3:
-                if not self.ax_zoomed:
-                    ax.set_position([0.1, 0.1, 0.85, 0.85])
-                    ax.set_xlabel("Frequency")
-                    ax.set_ylabel("Time")
-                    self.orig_position = ax.get_position()
-                    for axis in event.canvas.figure.axes:
-                       # Hide all the other axes...
-                       if axis is not ax:
-                           axis.set_visible(False)
-                    self.ax_zoomed=True
-                else:
-                    self.updatePlot()
-                    
-            else:
-                # No need to re-draw the canvas if it's not a left or right click
-                return
-            event.canvas.draw()
-            
-        self.fig_connect = self.sp_fig.canvas.mpl_connect('button_press_event', on_click)
-
         
         figtitle = '%s %s: %s -- %s'%(self.uv.telescope, self.uv.instrument, self.uv.source, self.uv.date_obs)
         for i in range(n_rows):
@@ -388,6 +532,8 @@ class InterFitsGui(QtGui.QWidget):
     def plot_autocorrs(self, axis=0, n_rows=4, n_cols=8):
         """ Plot autocorrelations for all antennas
         """
+        self.current_plot = 'multi'
+        self.ax_zoomed = False
         
         bls = self.uv.d_uv_data['BASELINE']
 
@@ -396,15 +542,15 @@ class InterFitsGui(QtGui.QWidget):
         bl_ids = [256*i + i for i in range(1,self.uv.n_ant+1)]
         bl_truths = np.array([(b in bl_ids) for b in bls])
         
-        print self.uv.d_uv_data['DATA'].shape
+        #print self.uv.d_uv_data['DATA'].shape
         #x_data    = self.d_uv_data['DATA'][bl_truths,0,0,:,0,axis]  # Baselines, freq and stokes
         x_data    = self.uv.d_uv_data['DATA'][bl_truths, 0,0,0,:,axis,:]
         x_cplx    = x_data[:,:,0] + 1j * x_data[:,:,1]
         
-        print x_cplx.shape
+        #print x_cplx.shape
         
         # Plot the figure
-        print self.uv.n_ant
+        #print self.uv.n_ant
         fig = self.sp_fig
         figtitle = '%s %s: %s -- %s'%(self.uv.telescope, self.uv.instrument, self.uv.source, self.uv.date_obs)
         for i in range(n_rows):
@@ -452,16 +598,19 @@ class InterFitsGui(QtGui.QWidget):
         
         if self.plot_select.currentIndex() == 0:
             self.plot_single_baseline(ref_ant, ref_ant2, axis=axis)
-        
+            
         if self.plot_select.currentIndex() == 1:
-            self.plot_autocorrs(axis=axis)
+            self.plot_single_baseline_dual_pol(ref_ant, ref_ant2, axis=axis)
         
         if self.plot_select.currentIndex() == 2:
-            self.plot_visibilities(plot_type='amp', axis=axis, ref_ant=ref_ant)
+            self.plot_autocorrs(axis=axis)
         
         if self.plot_select.currentIndex() == 3:
-            self.plot_visibilities(plot_type='phase', axis=axis, ref_ant=ref_ant)
+            self.plot_visibilities(plot_type='amp', axis=axis, ref_ant=ref_ant)
         
+        if self.plot_select.currentIndex() == 4:
+            self.plot_visibilities(plot_type='phase', axis=axis, ref_ant=ref_ant)
+            
         self.sp_canvas.draw()
         
     def updateSpinners(self):
@@ -474,12 +623,12 @@ class InterFitsGui(QtGui.QWidget):
         ref_ant = self.spin_ref_ant.value()
         ref_ant2 = self.spin_ref_ant2.value()
         
-        if self.plot_select.currentIndex() == 0:
+        if self.current_plot == 'single':
             self.spin_ref_ant2.setEnabled(True)
         else:
             self.spin_ref_ant2.setDisabled(True)
         
-        if self.plot_select.currentIndex() == 1:
+        if self.current_plot == 'multi':
             self.spin_ref_ant.setEnabled(False)
         else:
             self.spin_ref_ant.setEnabled(True)
