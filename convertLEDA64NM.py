58205bc492e880eab3e1f1f9f3f28dd11c3674e2
jaycedowell <jayce.dowell@gmail.com>
2014-02-19 13:04:54 -0800

    Added the center frequency in MHz to the filename to help avoid confusion.

--- convertLEDA64NM.py
+++ convertLEDA64NM.py
@@ -17,16 +17,24 @@ def main(args):
 		
 		# Build the output name
 		obsDate = datetime.strptime(uvw.date_obs, "%Y-%m-%dT%H:%M:%S")
-		outname = "%s_%s_%s.FITS_1" % (uvw.instrument, uvw.telescope, obsDate.strftime("%Y%m%d%H%M%S"))
-		print "Converting '%s' to '%s'..." % (filename, outname)
+		obsFreq = int((uvw.formatFreqs()).mean() / 1e6)
+		outname = "%s_%s_%s_%iMHz.FITS_1" % (uvw.instrument, uvw.telescope, obsDate.strftime("%Y%m%d%H%M%S"), obsFreq)
+		print "Converting '%s' to '%s'..." % (os.path.basename(filename), outname)
 		
-		# Add in the metadata
-		uvw.loadAntArr()
+		# Add in the UVW coordinates
 		uvw.generateUVW(src='ZEN', use_stored=False, update_src=True)
+		
+		# Apply the cable delays
 		uvw.apply_cable_delays()
+		
+		## Phase to zenith
 		#uvw.phase_to_src(src='ZEN', debug=False)
-		uvw.exportFitsidi(outname)
+		
+		# Verify
 		uvw.verify()
+		
+		# Save as FITS IDI
+		uvw.exportFitsidi(outname)
 
 
 if __name__ == "__main__":
